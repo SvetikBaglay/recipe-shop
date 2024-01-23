3071b3444fbe40afb23c537760978fd802469d4a
SvitlanaBahlai <conbka@gmail.com>
2024-01-23 10:51:15 +0200

    added list & book for recipe

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeBookComponent } from '../recipe-book/recipe-book.component';
+import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 
 @Component({
   templateUrl: './header.component.html',
   selector: 'app-header',
+  styleUrl: './header.component.css',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RecipeBookComponent,
+    ShoppingListComponent,
+  ],
 })
 export class HeaderComponent {}
